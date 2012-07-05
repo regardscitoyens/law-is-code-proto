73b7ca35a0ca98fce0ed42d849f8761f652161c9
Tangui Morlier <tangui@tangui.eu.org>
2012-07-05 16:09:55 +0200

    Génère sed

--- bin/pl_ajout_supprime_remplace.pl
+++ bin/pl_ajout_supprime_remplace.pl
@@ -5,20 +5,23 @@
 use utf8;
 
 while(<STDIN>) {
+    chomp;
 	$alinea = $1 if (/^(\d+)\t/);
 	$action = '';
-	while (/(([\{\[])[\+\-]([^\}\]]+)[\+\-]([\}\]]))/g) {
-		$tout = $1;
-		if ($2 eq '{') {
+	while (/([^\]\}]*)(([\{\[])[\+\-]([^\}\]]+)[\+\-]([\}\]]))([^\[\{]*)/g) {
+		$tout = $2;
+		$contexte = "$1|$6";
+		if ($3 eq '{') {
 			$action = 'ajout';	
-		}elsif ($4 eq '}') {
+		}elsif ($5 eq '}') {
 			$action = 'remplace';
 		}else{
-			$action = 'supprime';
+			$action = "supprime";
 		}
-		$mots = $3;
+		$mots = $4;
+		$contexte =~ s/\d+\t//;
 		$mots =~ s/\|/ /g;
 		$mots =~ s/[,\.;!?']/ /g;
-		print "$alinea\t$action\t$mots\t$tout\n";
+		print "$alinea\t$action\t$mots\t$tout\t$contexte\n";
 	}
 }
