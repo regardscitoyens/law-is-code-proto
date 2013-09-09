d1b03b178281829df0f6fac3a232558e541742e8
Tangui Morlier <tangui@tangui.eu.org>
2013-09-09 18:30:42 +0200

    Add info about amendements and interventions

--- bin/procedure2json.py
+++ bin/procedure2json.py
@@ -2,21 +2,43 @@
 # -*- coding=utf-8 -*-
 """Create a json from from a procedure csv one"""
 
-import sys
+import sys, os, re
 import simplejson as json
 import csv
+
+csvpath = sys.argv[1]
+projectdir = csvpath.replace('/procedure.csv', '')
 
 def row2dir(row):
     return row[3]+'_'+row[5].replace(' ', '')+'_'+row[6]+'_'+row[7]
 
 procedure = {'type': 'Normale'}
 steps = []
-with open(sys.argv[1], 'rb') as csvfile:
+with open(csvpath, 'rb') as csvfile:
     csvproc = csv.reader(csvfile, delimiter=';')
     for row in csvproc:
         step = {'date': row[10], 'stage': row[5], 'institution': row[6], 'source_url': row[8]}
         if (row[4] != 'EXTRA'):
             step['directory'] = row2dir(row)
+            try:
+                if (os.stat(projectdir+'/'+step['directory']+'/amendements/amendements.csv')):
+                    step['has_amendements'] = True
+                    step['amendement_directory'] = step['directory']+'/amendements'
+            except:
+                step['has_amendements'] = False
+            try:
+                intervention_dir = projectdir+'/'+step['directory']+'/interventions/'
+                if (os.stat(intervention_dir)):
+                    step['has_interventions'] = True
+            except:
+                step['has_interventions'] = False
+            if (step['has_interventions']):
+                files = []
+                for f in os.listdir(intervention_dir):
+                    if re.search('.json$', f):
+                        files.append(f.replace('.json', ''))
+                step['intervention_files'] = files
+                step['intervention_directory'] = step['directory']+'/interventions'
             step['step'] = row[7]
             step['resulting_text_directory'] =  row2dir(row)+'/texte'
             if ((row[3] != 'XX') and (int(row[3]) > 0)):
